089a0931b5828304a7e7612737ec9a10b1a2dc6d
Eryk Olliver <erykolliver@gmail.com>
2026-02-18 12:42:42 -0300

    config: paleta e escala tipografica ajustadas

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
+import { Heading } from "./components/text";
 
 export default function Home() {
   return (
     <main className="bg-black w-screen h-screen items-center flex flex-col gap-2 justify-center">
-      <h1 className="text-3xl uppercase border-b py-1 w-1/3 font-bold text-center border-gray-600">Eryk Olliver</h1>
-      <h1 className="text-xl text-gray-600 uppercase">Full-Stack Developer</h1>
+      <Heading level={1} className="text-white-pure  underline">Eryk Olliver</Heading>
     </main>
   );
 }
