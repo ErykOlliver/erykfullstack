67663cc81c9205d8414ea3b8f3aff8d4643590c9
Eryk Olliver <erykolliver@gmail.com>
2026-02-22 14:22:00 -0300

    fix: problema de session secret key resolvido

--- src/app/(admin)/dashboard/layout.tsx
+++ src/app/(admin)/dashboard/layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { authOptions } from '@/src/app/api/auth/[...nextauth]/route'
+import { authOptions } from '../../api/auth/[...nextauth]/route'
 import { getServerSession } from 'next-auth'
 import { redirect } from 'next/navigation'
 
